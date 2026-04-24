dc789ef72dd13f729d9de2f8cb5d6b844868f3bd
Nenad Bogdanovic <nenad.bogdanovic@elfak.rs>
2026-04-24 23:20:47 +0200

    add WebServer.cs skeleton

---
+++ TMDBMovieSearch/Server/WebServer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMDBMovieSearch.Server
+{
+    public class WebServer
+    {
+        private readonly HttpListener _listener;
+        private readonly string _prefix;
+
+        public WebServer(string prefix)
+        {
+            _prefix = prefix;
+            _listener = new HttpListener();
+            _listener.Prefixes.Add(prefix);
+        }
+
+        public void Start()
+        {
+            _listener.Start();
+            Console.WriteLine($"Server slusa na {_prefix}");
+
+            // petlja za osluskivanje
+            while (true)
+            {
+                HttpListenerContext context = _listener.GetContext();
+                // ovde ces proslediti zahtev na obradu
+            }
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+        }
+    }
+}
