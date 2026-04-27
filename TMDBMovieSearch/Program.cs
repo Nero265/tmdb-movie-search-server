0d3302496a6121f6a18a141435afd407bdb39fbb
Nenad Bogdanovic <nenad.bogdanovic@elfak.rs>
2026-04-27 14:07:26 +0200

    Added WebServer with HttpListener and ThreadPool request handling

--- TMDBMovieSearch/Program.cs
+++ TMDBMovieSearch/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TMDBMovieSearch.Server;
+
+var server = new WebServer("http://localhost:5000/");
+server.Start();
--- TMDBMovieSearch/Server/WebServer.cs
+++ TMDBMovieSearch/Server/WebServer.cs
@@ -28,13 +28,49 @@ namespace TMDBMovieSearch.Server
             while (true)
             {
                 HttpListenerContext context = _listener.GetContext();
-                // ovde ces proslediti zahtev na obradu
+
+                HttpListenerContext capturedContext = context; //captured-variable
+
+                ThreadPool.QueueUserWorkItem(state =>
+                {
+                    HandleRequest(capturedContext);
+                });
             }
         }
 
         public void Stop()
         {
             _listener.Stop();
+            Console.WriteLine("Server je zaustavljen.");
+        }
+
+        private void HandleRequest(HttpListenerContext context)
+        {
+            Console.WriteLine($"[REQUEST] {context.Request.HttpMethod} {context.Request.Url}");
+
+            string? query = context.Request.QueryString["query"]; //citamo query parametar
+            
+            if (string.IsNullOrEmpty(query))
+            {
+                SendResponse(context, 400, "Nedostaje query parametar. Primer: /search?query=Project+Hail+Mary");
+                return;
+            }
+
+            //placeholder za sad -posle ide TMDBService poziv
+            SendResponse(context, 200, $"Primljen zahtev za: {query}");
+        }
+
+        private void SendResponse(HttpListenerContext context, int statusCode, string message)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(message); //HTTP salje bytes
+
+            context.Response.StatusCode = statusCode;
+            context.Response.ContentType = "application/json; charset=utf-8";
+            context.Response.ContentLength64 = buffer.Length;
+
+            context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+
+            context.Response.OutputStream.Close();
         }
     }
 }
